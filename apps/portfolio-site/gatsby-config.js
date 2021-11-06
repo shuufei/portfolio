f3fe638244aca447539ca60f694295f2f2e39214
shuufei <digital.shuufei@gmail.com>
2021-11-06 11:02:23 +0900

    feat: connect contentful

--- apps/portfolio-site/gatsby-config.js
+++ apps/portfolio-site/gatsby-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
     title: `SHUFEI`,
@@ -40,6 +44,13 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/logo.svg`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.SPACE_ID,
+        accessToken: process.env.ACCESS_TOKEN,
       },
     },
   ],
