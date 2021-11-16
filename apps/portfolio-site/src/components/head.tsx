183585733c4a103dc8c80a20cad236cea02a390c
shuufei <digital.shuufei@gmail.com>
2021-11-16 22:45:23 +0900

    fix: fixed ogp tag

--- apps/portfolio-site/src/components/head.tsx
+++ apps/portfolio-site/src/components/head.tsx
@@ -12,12 +12,18 @@ export const Head = () => {
       <meta property="og:type" content="article" />
       <meta property="og:title" content="S H U F E I" />
       <meta property="og:description" content="Shufei Poirtfolio" />
-      <meta property="og:image" content="/images/thumbnail.png" />
+      <meta
+        property="og:image"
+        content="https://competent-lichterman-6f12f6.netlify.app/images/thumbnail.png"
+      />
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:creator" content="@digital_fei" />
       <meta name="twitter:title" content="S H U F E I" />
       <meta name="twitter:description" content="Shufei Portfolio" />
-      <meta name="twitter:image" content="/images/thumbnail.png" />
+      <meta
+        name="twitter:image"
+        content="https://competent-lichterman-6f12f6.netlify.app/images/thumbnail.png"
+      />
     </Helmet>
   );
 };
