53c016bf62c4837ccf09bb369e66f3f5878928c7
shuufei <digital.shuufei@gmail.com>
2021-11-16 23:12:20 +0900

    fix: fixed ogp twitter:card tag

--- apps/portfolio-site/src/components/head.tsx
+++ apps/portfolio-site/src/components/head.tsx
@@ -15,7 +15,8 @@ export const Head = () => {
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
-      <meta name="twitter:card" content="summary_large_image" />
+      {/* <meta name="twitter:card" content="summary_large_image" /> */}
+      <meta name="twitter:card" content="photo" />
       <meta name="twitter:creator" content="@digital_fei" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
