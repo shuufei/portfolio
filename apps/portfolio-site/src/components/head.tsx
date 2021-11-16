5332e8b73da5e96718ef737550b4e30ab95b6148
shuufei <digital.shuufei@gmail.com>
2021-11-16 22:30:39 +0900

    fix: fixed ogp tag

--- apps/portfolio-site/src/components/head.tsx
+++ apps/portfolio-site/src/components/head.tsx
@@ -5,6 +5,19 @@ export const Head = () => {
     <Helmet>
       <meta name="description" content="Shufei Poirtfolio" />
       <meta name="image" content="/images/thumbnail.png" />
+      <meta
+        property="og:url"
+        content="https://competent-lichterman-6f12f6.netlify.app/"
+      />
+      <meta property="og:type" content="article" />
+      <meta property="og:title" content="S H U F E I" />
+      <meta property="og:description" content="Shufei Poirtfolio" />
+      <meta property="og:image" content="/images/thumbnail.png" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:creator" content="@digital_fei" />
+      <meta name="twitter:title" content="S H U F E I" />
+      <meta name="twitter:description" content="Shufei Portfolio" />
+      <meta name="twitter:image" content="/images/thumbnail.png" />
     </Helmet>
   );
 };
