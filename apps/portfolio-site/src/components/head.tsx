79fd703cce7ebbf098194a3ebb5b3903240f2c35
shuufei <digital.shuufei@gmail.com>
2021-11-16 22:57:04 +0900

    fix: fixed ogp tag

--- apps/portfolio-site/src/components/head.tsx
+++ apps/portfolio-site/src/components/head.tsx
@@ -1,29 +1,25 @@
 import Helmet from 'react-helmet';
 
 export const Head = () => {
+  const title = 'S H U F E I';
+  const description = 'Shufei Poirtfolio';
+  const url = 'https://competent-lichterman-6f12f6.netlify.app/';
+  const image = `${url}/images/thumbnail.png`;
   return (
     <Helmet>
-      <meta name="description" content="Shufei Poirtfolio" />
-      <meta name="image" content="/images/thumbnail.png" />
-      <meta
-        property="og:url"
-        content="https://competent-lichterman-6f12f6.netlify.app/"
-      />
+      <meta name="description" content={description} />
+      <meta name="image" content={image} />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={url} />
       <meta property="og:type" content="article" />
-      <meta property="og:title" content="S H U F E I" />
-      <meta property="og:description" content="Shufei Poirtfolio" />
-      <meta
-        property="og:image"
-        content="https://competent-lichterman-6f12f6.netlify.app/images/thumbnail.png"
-      />
-      <meta name="twitter:card" content="summary" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+      <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content="@digital_fei" />
-      <meta name="twitter:title" content="S H U F E I" />
-      <meta name="twitter:description" content="Shufei Portfolio" />
-      <meta
-        name="twitter:image"
-        content="https://competent-lichterman-6f12f6.netlify.app/images/thumbnail.png"
-      />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image:src" content={image} />
     </Helmet>
   );
 };
