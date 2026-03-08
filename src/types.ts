b7437b048dfe0cbe9966fdb8980d68607981016b
shuufei <digital.shuufei@gmail.com>
2026-03-08 23:26:05 +0900

    add: draft

---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export type Category = 'Web' | 'Book' | 'Typography';
+
+export const CATEGORY_COLORS: Record<Category, string> = {
+  Web:        '#8BEEFF',
+  Book:       '#A8FF8B',
+  Typography: '#FFEE8B',
+};
