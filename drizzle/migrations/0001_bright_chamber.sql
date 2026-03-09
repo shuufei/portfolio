4c66e872aee470180967efb8331296122b3c589f
shuufei <digital.shuufei@gmail.com>
2026-03-10 00:19:06 +0900

    tmp: migrate検証

---
+++ drizzle/migrations/0001_bright_chamber.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `works` DROP COLUMN `description`;
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,11 +1,10 @@
-import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
+import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
 
-export const works = sqliteTable('works', {
-  id: integer('id').primaryKey({ autoIncrement: true }),
-  title: text('title').notNull(),
-  slug: text('slug').notNull().unique(),
-  description: text('description'),
-  imageUrl: text('image_url'),
-  tags: text('tags'),
-  createdAt: integer('created_at', { mode: 'timestamp' }).notNull(),
+export const works = sqliteTable("works", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  title: text("title").notNull(),
+  slug: text("slug").notNull().unique(),
+  imageUrl: text("image_url"),
+  tags: text("tags"),
+  createdAt: integer("created_at", { mode: "timestamp" }).notNull(),
 });
