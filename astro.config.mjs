6b9a98821f6c7ddd8ff4b74f7c8dd10e1e12bd4a
shuufei <digital.shuufei@gmail.com>
2026-03-09 23:43:13 +0900

    add: cloudflare plugin for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
-import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
+import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
+  output: "server",
   adapter: cloudflare(),
 });
